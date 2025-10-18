8e8234e75859ea2361e48e145779181dc438ccfa
Kerem ARAN <arankerem05@outlook.com>
2025-10-18 17:05:25 +0300

    v0.01

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
     path('', views.content_list, name="content_list"),                         #İçerik listesini gösteren sayfa url'si
 
-    path('content/<int:Content_id>/',views.content_detail,name="content_detail"),   #İçerik detaylarını gösteren sayfa url'si
+    path('content/<int:content_id>/',views.content_detail,name="content_detail"),   #İçerik detaylarını gösteren sayfa url'si
 
 ]
