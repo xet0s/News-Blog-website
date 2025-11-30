ff1d96a0ebd47f018b334e4730b3f26d72b636b0
Kerem ARAN <arankerem05@outlook.com>
2025-11-30 22:26:48 +0300

    HOTFIX v1.1

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog_website.urls'
@@ -86,7 +86,8 @@ DATABASES = { #Veri Tabanı bağlantısı
     'HOST': '31.186.11.110',
     'PORT': '3306',
     'OPTIONS': {
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        "charset":"utf8mb4"
         }
     }
 }
